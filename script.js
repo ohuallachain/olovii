9f3d4d6321741e9e7df7a140377d642a8d8f6b18
Darragh O hUallachain <darraghohuallachain@gmail.com>
2025-02-12 16:41:25 +0000

    all 3

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 "use strict"
 
+document.querySelector("h1").addEventListener("click", function() {
+    document.querySelector("h1").style.color = "white";
+    document.querySelector("h1").style.font = "3rem";
+
+
+})
