170f30ae206265186867aac5209e5c35d5258763
Darragh O hUallachain <darraghohuallachain@gmail.com>
2025-02-12 18:11:34 +0000

    js update

--- script.js
+++ script.js
@@ -2,7 +2,5 @@
 
 document.querySelector("h1").addEventListener("click", function() {
     document.querySelector("h1").style.color = "white";
-    document.querySelector("h1").style.font = "3rem";
-
 
 })
